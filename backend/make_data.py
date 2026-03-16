a6fbadc7bf6ebefb82c1f561284d4779e099b25a
SeungMin <dolkuji1@naver.com>
2026-03-16 21:25:37 +0900

    dataset

--- backend/make_data.py
+++ backend/make_data.py
@@ -3,6 +3,7 @@ import whisper
 from datasets import load_dataset
 import torch
 
+
 device="mps" if torch.backends.mps.is_available() else "cuda"
 model=whisper.load_model("turbo",device=device) #모델설정(transcript할 모델)
 
@@ -10,24 +11,22 @@ rows = []
 
 # 예시: 공개 데이터셋 한 부분을 불러온 뒤 일부만 사용
 # 실제 컬럼명은 데이터셋마다 다를 수 있으니 print(sample)로 먼저 확인
-ds = load_dataset(
-    "cheulyop/ksponspeech",
-    split="validation",
-    trust_remote_code=True
-)
 
-for i, sample in enumerate(ds.select(range(100))):
-    audio_path = sample["audio"]["path"]
-    target_text = sample["sentence"].strip()   # 컬럼명은 확인 필요
+ds = load_dataset("DragonLine/ksponspeech", split="train[:100]")
+print(ds[0])
 
-    result = model.transcribe(audio_path, language="ko")
-    input_text = result["text"].strip()
+# for i, sample in enumerate(ds.select(range(100))):
+#     audio_path = sample["audio"]["path"]
+#     target_text = sample["sentence"].strip()   # 컬럼명은 확인 필요
 
-    rows.append({
-        "input_text": input_text,
-        "target_text": target_text
-    })
+#     result = model.transcribe(audio_path, language="ko")
+#     input_text = result["text"].strip()
 
-df = pd.DataFrame(rows)
-df.to_csv("train.csv", index=False, encoding="utf-8-sig")
-print(df.head())
+#     rows.append({
+#         "input_text": input_text,
+#         "target_text": target_text
+#     })
+
+# df = pd.DataFrame(rows)
+# df.to_csv("train.csv", index=False, encoding="utf-8-sig")
+# print(df.head())
