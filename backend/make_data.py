78ead80d19d6cb8efd290aa26257b0b30d387b79
SeungMin <dolkuji1@naver.com>
2026-03-16 22:39:06 +0900

    data

--- backend/make_data.py
+++ backend/make_data.py
@@ -5,57 +5,59 @@ from datasets import load_dataset
 import torch
 import numpy as np
 
+name=["train","vaild","test"]
 cache_path = r"C:\Users\user\Documents\ksponspeech_data"
 
-train_ds = load_dataset(
-    "DragonLine/ksponspeech",
-    split="train[:100]",
-    cache_dir=cache_path
-)
-
-device="mps" if torch.backends.mps.is_available() else "cuda"
-model = whisper.load_model("large-v3",device=device)
-
-def clean_transcript(text: str) -> str:
-    text = text.strip()
-    text = text.replace("\n", " ")
-    text = re.sub(r"[a-zA-Z]/\s*", "", text)   # b/ 같은 표기 제거
-    text = text.replace("*", "")               # * 제거
-    text = re.sub(r"\s+", " ", text).strip()   # 공백 정리
-    return text
-
-rows = []
-
-for i, sample in enumerate(train_ds):
-    audio_array = sample["audio"]["array"].astype(np.float32)
-    target_text = clean_transcript(sample["transcripts"])
-
-    result = model.transcribe(
-        audio_array,
-        language="ko",
-        task="transcribe",
-        fp16=False,
-        temperature=0.0,
-        condition_on_previous_text=False,
-        verbose=False,
+for k in name:
+    train_ds = load_dataset(
+        "DragonLine/ksponspeech",
+        split=k,
+        cache_dir=cache_path
     )
 
-    input_text = result["text"].strip()
+    device="mps" if torch.backends.mps.is_available() else "cuda"
+    model = whisper.load_model("large-v3",device=device)
 
-    rows.append({
-        "input_text": input_text,
-        "target_text": target_text
-    })
+    def clean_transcript(text: str) -> str:
+        text = text.strip()
+        text = text.replace("\n", " ")
+        text = re.sub(r"[a-zA-Z]/\s*", "", text)   # b/ 같은 표기 제거
+        text = text.replace("*", "")               # * 제거
+        text = re.sub(r"\s+", " ", text).strip()   # 공백 정리
+        return text
 
-    print(f"{i+1}/{len(train_ds)}")
-    print("[INPUT ]", input_text)
-    print("[TARGET]", target_text)
-    print("-" * 50)
+    rows = []
 
-df = pd.DataFrame(rows)
-df.to_csv(
-    r"C:\Users\user\Documents\ksponspeech_data\train_pairs.csv",
-    index=False,
-    encoding="utf-8-sig"
-)
-print("saved: train_pairs.csv")
+    for i, sample in enumerate(train_ds):
+        audio_array = sample["audio"]["array"].astype(np.float32)
+        target_text = clean_transcript(sample["transcripts"])
+
+        result = model.transcribe(
+            audio_array,
+            language="ko",
+            task="transcribe",
+            fp16=False,
+            temperature=0.0,
+            condition_on_previous_text=False,
+            verbose=False,
+        )
+
+        input_text = result["text"].strip()
+
+        rows.append({
+            "input_text": input_text,
+            "target_text": target_text
+        })
+
+        print(f"{i+1}/{len(train_ds)}")
+        print("[INPUT ]", input_text)
+        print("[TARGET]", target_text)
+        print("-" * 50)
+
+    df = pd.DataFrame(rows)
+    df.to_csv(
+        fr"C:\Users\user\Documents\ksponspeech_data\{k}_pairs.csv",
+        index=False,
+        encoding="utf-8-sig"
+    )
+    print(f"saved: {k}_pairs.csv")
