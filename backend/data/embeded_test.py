ed9c58b782dc08bace794a9cb5182caec55d3640
SeungMin <dolkuji1@naver.com>
2026-03-21 18:01:22 +0900

    +device

--- backend/data/embeded_test.py
+++ backend/data/embeded_test.py
@@ -1,6 +1,8 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("BAAI/bge-m3")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("device: ",device)
+model = SentenceTransformer("BAAI/bge-m3",device=device)
 
 def get_embedding(text: str):
     embedding = model.encode(text, normalize_embeddings=True)
