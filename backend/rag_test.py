6d47c969db2f5bf91997e15d364dae20c9ab04da
SeungMin <dolkuji1@naver.com>
2026-03-23 19:25:18 +0900

    end

--- backend/rag_test.py
+++ backend/rag_test.py
@@ -1,4 +1,28 @@
+import json
+from llama_index.core import Document, StorageContext, VectorStoreIndex
 from llama_index.vector_stores.postgres import PGVectorStore
+
+file_path="/backend/data/transcripts/60db78d8-10fe-48f1-8f05-9d31a8f561a5.jsonl"
+documents=[]
+
+with open(file_path, "r", encoding="utf-8") as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+
+        item = json.loads(line)
+
+        doc = Document(
+            text=item["text"],
+            metadata={
+                "session_id": item["session_id"],
+                "start_time": item["start_time"],
+                "end_time": item["end_time"],
+                "raw_text": item["raw_text"],
+            }
+        )
+        documents.append(doc)
 
 vector_store = PGVectorStore.from_params(
     database="rag",
@@ -10,4 +34,14 @@ vector_store = PGVectorStore.from_params(
     embed_dim=1024
 )
 
-print("PGVectorStore 연결 객체 생성 완료")
+storage_context = StorageContext.from_defaults(
+    vector_store=vector_store
+)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+    show_progress=True
+)
+
+print("완료")
