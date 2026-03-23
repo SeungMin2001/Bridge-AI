205ae62fb7d3274cc64777e15bf96cba0b2ddb54
SeungMin <dolkuji1@naver.com>
2026-03-23 19:25:57 +0900

    change path

--- backend/rag_test.py
+++ backend/rag_test.py
@@ -2,7 +2,7 @@ import json
 from llama_index.core import Document, StorageContext, VectorStoreIndex
 from llama_index.vector_stores.postgres import PGVectorStore
 
-file_path="/backend/data/transcripts/60db78d8-10fe-48f1-8f05-9d31a8f561a5.jsonl"
+file_path="/data/transcripts/60db78d8-10fe-48f1-8f05-9d31a8f561a5.jsonl"
 documents=[]
 
 with open(file_path, "r", encoding="utf-8") as f:
