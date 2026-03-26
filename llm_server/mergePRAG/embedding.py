38f5c292842ddcd5024f95a8f8c69cc0cc8f08d1
SeungMin <dolkuji1@naver.com>
2026-03-26 20:28:38 +0900

    test

---
+++ llm_server/mergePRAG/embedding.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+def embedding(MODEL_NAME):
+    device = "cuda"
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(device)
+
+    text = "This is a passage for hypernetwork input."
+
+    inputs = tokenizer(
+        text,
+        return_tensors="pt",
+        truncation=True,
+        padding=True
+    )
+
+    input_ids = inputs["input_ids"].to(device)
+    attention_mask = inputs["attention_mask"].to(device)
+
+    # Qwen의 입력 임베딩 사용
+    embeddings = model.get_input_embeddings()(input_ids)   # [B, T, d]
+
+    print(embeddings.shape)
