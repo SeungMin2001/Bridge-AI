dfd64787c2cde1558c893face85ba54900c85d4c
SeungMin <dolkuji1@naver.com>
2026-03-26 20:54:58 +0900

    test

--- llm_server/mergePRAG/hypernetwork.py
+++ llm_server/mergePRAG/hypernetwork.py
@@ -2,10 +2,10 @@ import json
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from llm_server.run_model import run_model
-from mergePRAG.embedding import embedding
+import embedding
 
 model,tokenizer=run_model()
 
-embedded=embedding(model,tokenizer,text="test")
+embedded=embedding.embedding(model,tokenizer,text="test")
 
 print(embedded)
