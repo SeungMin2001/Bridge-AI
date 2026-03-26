45eb625c68e15659a82fe71f9340ad50c01dfb15
SeungMin <dolkuji1@naver.com>
2026-03-26 21:16:50 +0900

    test

--- llm_server/mergePRAG/hypernetwork.py
+++ llm_server/mergePRAG/hypernetwork.py
@@ -5,13 +5,16 @@ from llm_server.run_model import run_model
 from .embedding import embedding
 from .pooling import AttentivePooling
 
+
+
 model,tokenizer=run_model() #모델 실행(qwen 3.5 9B)
+device = next(model.parameters()).device # cuda
 
 text="test"
 
-embedded=embedding(model,tokenizer,text) #embedding from qwen 3.5
+embedded=embedding(model,tokenizer,text).to(device) #embedding from qwen 3.5
 
-pooling=AttentivePooling(model.config.hidden_size)
+pooling=AttentivePooling(model.config.hidden_size).to(device)
 res=pooling.forward(embedded)
 
 print(res)
