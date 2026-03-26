58dd8dbf7f7fabdbaf511955a006043c2318b6cc
SeungMin <dolkuji1@naver.com>
2026-03-26 20:56:28 +0900

    test

--- llm_server/mergePRAG/hypernetwork.py
+++ llm_server/mergePRAG/hypernetwork.py
@@ -2,7 +2,7 @@ import json
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from llm_server.run_model import run_model
-import embedding
+from .embedding import embedding
 
 model,tokenizer=run_model()
 
