1190fae200beaed90c698e2a8d80b2ca3c051b71
SeungMin <dolkuji1@naver.com>
2026-03-29 17:45:45 +0900

    preparation

--- llm_server/correction/KoBART.py
+++ llm_server/correction/KoBART.py
@@ -1,6 +1,31 @@
 from transformers import AutoTokenizer, BartForConditionalGeneration
-
+from make_dataset import dataset
 model_name = "gogamza/kobart-base-v2"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
+
+def preprocess_function(examples):
+    model_inputs = tokenizer(
+        examples["input_text"],
+        max_length=128,
+        truncation=True
+    )
+
+    labels = tokenizer(
+        text_target=examples["target_text"],
+        max_length=128,
+        truncation=True
+    )
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
+tokenized_datasets = dataset.map(
+    preprocess_function,
+    batched=True
+)
+
+print(tokenized_datasets["train"][0].keys())
+print(tokenized_datasets["train"][0])
