fb0973086d6085993106aca29949c903b82dd5f9
SeungMin <dolkuji1@naver.com>
2026-03-29 17:51:14 +0900

    train

--- llm_server/correction/KoBART.py
+++ llm_server/correction/KoBART.py
@@ -1,5 +1,8 @@
 from transformers import AutoTokenizer, BartForConditionalGeneration
 from make_dataset import dataset
+from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
 model_name = "gogamza/kobart-base-v2"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -27,5 +30,37 @@ tokenized_datasets = dataset.map(
     batched=True
 )
 
-print(tokenized_datasets["train"][0].keys())
-print(tokenized_datasets["train"][0])
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer=tokenizer,
+    model=model
+)
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./kobart_correction",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    logging_steps=100,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    learning_rate=5e-5,
+    weight_decay=0.01,
+    predict_with_generate=True,
+    fp16=True
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    processing_class=tokenizer,
+    data_collator=data_collator
+)
+
+trainer.train()
+
+save_path = r"C:\Users\user\Documents\last_project\models\kobart_correction"
+
+trainer.save_model(save_path)
+tokenizer.save_pretrained(save_path)
