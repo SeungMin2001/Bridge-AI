1fdc18b18c1bd785ee9f6e9c15c8679a70588900
SeungMin <dolkuji1@naver.com>
2026-04-02 19:26:17 +0900

    passward

--- backend/db.py
+++ backend/db.py
@@ -6,7 +6,7 @@ DB_CONFIG = {
     "port": int(os.getenv("DB_PORT", 5432)),
     "database": os.getenv("DB_NAME", "shin"),
     "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "password": os.getenv("DB_PASSWORD", "1234"),
 }
 
 _pool = None
