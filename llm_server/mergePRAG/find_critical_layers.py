993b723e6f38e09b06710beb0ec6bf9a4861d621
SeungMin <dolkuji1@naver.com>
2026-04-15 20:19:01 +0900

    test

--- llm_server/mergePRAG/find_critical_layers.py
+++ llm_server/mergePRAG/find_critical_layers.py
@@ -23,9 +23,9 @@ from .hypernetwork import HyperNetwork
 from .train import MergePRAGDataset, compute_loss, make_hook, tokenize_qa
 
 OUTPUT_PATH = "llm_server/mergePRAG/critical_layers.json"
-SCAN_MAX_TRAIN_SAMPLES = 512
-SCAN_MAX_VAL_SAMPLES = 128
-SCAN_STEPS = 300
+SCAN_MAX_TRAIN_SAMPLES = 128
+SCAN_MAX_VAL_SAMPLES = 32
+SCAN_STEPS = 80
 LR = 1e-4
 LR_MIN = 1e-6
 
