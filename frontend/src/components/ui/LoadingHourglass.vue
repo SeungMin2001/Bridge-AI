6b3db59bf1b0583ab80aa70231d6fe55c03ca63d
shinchangyoung <toyoaki800@gmail.com>
2026-05-21 10:24:33 +0900

    코드 수정

--- frontend/src/components/ui/LoadingHourglass.vue
+++ frontend/src/components/ui/LoadingHourglass.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from 'vue'
-import lottie from 'lottie-web/build/player/lottie_light'
+//import lottie from 'lottie-web/build/player/lottie_light'
+import lottie from 'lottie-web/build/player/lottie_light.js'
+
 
 const props = defineProps({
   src: { type: String, default: '/animations/Remix%20Hourglass.json' },
